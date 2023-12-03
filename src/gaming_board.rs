5bceebc8797ed6f1b204dcc0942a74eb25f0f7a9
Faluminus <semradfilip@email.cz>
2023-12-03 12:37:59 +0200

    Started working on gaming board

--- src/gaming_board.rs
+++ src/gaming_board.rs
@@ -1,10 +1,35 @@
 
 mod gaming_board{
+    use std::collections::HashMap;
+
     struct Board{
+        map:HashMap<i32,char>,
         width:i32,
         height:i32
     }
     impl Board{
+        pub fn new(&mut self, width:i32,height:i32,wall_preset:char,space_preset:char){
+            self.width = width;
+            self.height = height;
 
+            height.drop();
+            let mut height = 0;
+
+            let mut map:HashMap<i32,char> = HashMap::new();
+            for i in 0..self.width*self.height{
+                if i == 0 || width==i {
+                    map.insert(i,wall_preset);
+                    if width== i{
+                        height += 1;
+                    }
+                }else{
+                    if height== 0 || height == self.height {
+                        map.insert(i, wall_preset);
+                    }else{
+                        map.insert(i,space_preset);
+                    }
+                }
+            }
+        }
     }
 }
--- src/snake/objects.rs
+++ src/snake/objects.rs
@@ -1,25 +1,32 @@
 pub mod objects{
     struct Objects{
         length:i32,
+        snake:bool,
         wall:bool
     }
     impl Objects{
         pub fn new(&mut self , length:i32){
             self.length = length;
+            self.snake = false;
             self.wall = false;
         }
-        pub fn colision_check(&mut self,colide:char,apple_preset:char,wall_preset:char){
+        pub fn colision_check(&mut self,colide:char,apple_preset:char,wall_preset:char,snake_preset:char){
             if colide==apple_preset{
                 self.apple_fn();
             }else if colide==wall_preset{
                 self.wall_fn();
+            }else if colide==snake_preset{
+                self.twist_your_neck();
             }
         }
-        fn apple_fn(&mut self){
+        fn eat_apple(&mut self){
             self.length +=1;
         }
-        fn wall_fn(&mut self){
+        fn kiss_wall(&mut self){
             self.wall = true;
+        }
+        fn twist_your_neck(&mut self){
+            self.snake = true;
         }
     }
 }
